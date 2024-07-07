e4d24db2914db411bcf720d1d02fcf62c4397a1f
aman304gupta <aman304gupta@gmail.com>
2024-07-07 14:27:34 -0700

    Added basic layout/pages

---
+++ src/app/(app)/app/dashboard/page.tsx
@@ -0,0 +1,15 @@
+import Branding from "@/components/branding";
+import Stats from "@/components/stats";
+import React from "react";
+
+export default function Page() {
+  return (
+    <main>
+      <div className="flex items-center justify-between text-white py-8">
+        <Branding />
+
+        <Stats />
+      </div>
+    </main>
+  );
+}
